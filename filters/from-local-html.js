380973d50756e39f0e0a2b8333d885966dc96c96
Mr. Black <gaia@national.shitposting.agency>
2015-08-14 03:04:37 +0200

    Added filters to read the input from a local markdown or html file.

---
+++ filters/from-local-html.js
@@ -0,0 +1,18 @@
+var cheerio = require('cheerio');
+var marked = require('marked');
+var fs = require('fs');
+
+function apply(params, next)
+{
+    var chap = params.chap;
+    var md = fs.readFileSync(__dirname + '/../' + chap.url, encoding = 'utf-8');
+
+    console.log('[\033[92mLoading\033[0m] ' + chap.url);
+    chap.dom.load(marked(md), { decodeEntities: false });
+    next();
+}
+
+module.exports =
+{
+    apply: apply
+};
