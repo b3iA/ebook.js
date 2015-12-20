5096412ef0ebbe59fdd161643ee595948f6874d2
Mr. Black <gaia@national.shitposting.agency>
2015-12-20 11:53:18 +0100

    Updated ebook.js:
    * Added unicode HTML character reference decoding
    * Better errors
    * Added additional support for multiple named and
      per-chapter referenced filter chains. This is
      useful for mixed-source or variably filtered
      content.

--- ebook.js
+++ ebook.js
@@ -1,20 +1,50 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 
+var ERROR_TAG = '[\033[91mError\033[0m]: ';
+
 if(process.argv.length < 3)
 {
     console.log('Usage: ebook.js <spec.json>');
     return;
 }
 
+function decode_cr(cr)
+{
+	var cp = 0;
+	
+	if(cr[2] === 'x') // Hex?
+		cp = parseInt(cr.substr(3, cr.length - 2), 16);
+	else
+		cp = parseInt(cr.substr(2, cr.length - 2), 10);
+		
+	return String.fromCodePoint(cp);
+}
+
+// Decode all HTML character references to unicode.
+function decode_crs(s)
+{
+	var i = -1;
+	var ls = s;
+	
+	while((i = ls.search(/&#.*;/)) > -1)
+	{
+		var ni = ls.indexOf(';', i);
+		
+		ls = ls.substr(0, i) + decode_cr(ls.substr(i, ni - i + 1)) + ls.substr(ni + 1);
+	}
+	
+	return ls;
+}
+
 function unescape_html(html)
 {
-    return html.replace(/&amp;/g, '&')
-    	       .replace(/&quot;/g, '"')
-    	       .replace(/&apos;/g, '\'')
-    	       .replace(/&#39;/g, '\'')
-    	       .replace(/&amp;#39;/g, '\'')
-    	       .replace(/&amp;/g, '&');
+    return decode_crs(html.replace(/&amp;/g, '&'))
+                          .replace(/&quot;/g, '"')
+    	                  .replace(/&apos;/g, '\'')
+    	                  .replace(/&#39;/g, '\'')
+    	                  .replace(/&amp;#39;/g, '\'')
+    	                  .replace(/&amp;/g, '&');
 }
 
 function FilterManager()
@@ -38,7 +68,7 @@ FilterManager.prototype.get = function(fid)
 
     if(!filter)
     {
-        console.log('No such filter: ' + fid);
+        console.log(ERROR_TAG + 'No such filter: ' + fid);
         process.exit();
     }
 
@@ -67,14 +97,14 @@ function Finalize(params)
             Sequence(ops, params);
         }
         else
-            console.log('[\033[91mError\033[0m]: Unable to interpret the output filter reference. It must be either a string or array of strings.');
+            console.log(ERROR_TAG + 'Unable to interpret the output filter reference. It must be either a string or array of strings.');
     }
 }
 
 function Sequence(ops, params)
 {
     if(ops.length < 2)
-        throw new Exception('Cannot create a sequence of less than two operations.');
+        throw new Exception(ERROR_TAG + 'Cannot create a sequence of less than two operations.');
 
     var last = function(params) { return function() { Finalize(params); }; }(params);
 
@@ -95,19 +125,61 @@ spec.loaded = 0;
 
 for(var i = 0; i < spec.contents.length; i++)
 {
+	var chap = spec.contents[i];
     var params = {
         spec: spec,
-        chap: spec.contents[i],
+        chap: chap,
     	unescape_html: unescape_html
     };
+
+    if(typeof(chap.title) !== 'string')
+    {
+    	console.log(ERROR_TAG + 'Each chapter must contain a "title" property (string).');
+    	return;
+    }
+    
+    if(typeof(chap.src) !== 'string')
+    {
+    	console.log(ERROR_TAG + 'Each chapter must contain a "src" property (string).');
+    	return;
+    }
 
     params.chap.id = '' + i;
     params.chap.dom = cheerio.load('');
 
     var ops = [];
-
-    for(var fi = 0; fi < spec.filters.length; fi++)
-        ops.push(filter_mgr.get(spec.filters[fi]));
-
+	var filter_type = Object.prototype.toString.call(spec.filters);
+	
+    if(filter_type === '[object Array]')
+    {
+		for(var fi = 0; fi < spec.filters.length; fi++)
+		    ops.push(filter_mgr.get(spec.filters[fi]));
+    }
+	else if(filter_type === '[object Object]')
+	{
+		
+		if(typeof(chap.filters) !== 'string')
+		{
+			console.log(ERROR_TAG + 'In "' + chap.title + '": When a collection of filters is specified, each chapter must also specify witch filter chain to use.');
+			return;
+		}
+		
+		if(!(chap.filters in spec.filters))
+		{
+			console.log(ERROR_TAG + 'In "' + chap.title + '"; Cannot resolve the filter chain "' + chap.filters + '".');
+			return;
+		}
+		
+		var filters = spec.filters[chap.filters];
+		
+		for(var fi = 0; fi < filters.length; fi++)
+		    ops.push(filter_mgr.get(filters[fi]));
+	}
+	else
+	{
+		console.log(ERROR_TAG + 'Unsupported filter chain type "' + filter_type + '".');
+		return;
+	}
+		
     Sequence(ops, params);
 }
