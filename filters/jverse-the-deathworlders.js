627207de0fd0128f328cc82933d10c1c29af1695
Mr. Black <gaia@national.shitposting.agency>
2015-12-20 11:58:11 +0100

    Slighty improved content filter for The Deathworlders.

--- filters/jverse-the-deathworlders.js
+++ filters/jverse-the-deathworlders.js
@@ -12,6 +12,41 @@ function apply(params, next)
             p.remove();
     });
 
+    $('p').each(function(i, e)
+	{
+        var p = $(e);
+        
+        if(p.text().trim() === '')
+        	p.remove();
+	});
+	
+    var end_m = /^end (chapter|part) \d/i;
+    
+    $('p, p strong').each(function(i, e)
+    {
+        var p = $(e);
+        var t = p.text();
+		var l = t.toLowerCase();
+		
+    	if(l.indexOf('++end chapter') === 0 || 
+    	   l.indexOf('++end of chapter') === 0)
+    	{
+    		p.nextAll().remove();
+    		p.remove();
+    	}
+		
+		if(l.search(end_m) === 0)
+    		p.remove();
+    	
+	    if(params.chap.title === 'Deliverance')
+	    {
+		    if(t === 'Four years previously.')
+		    	p.parent().html('<strong>Four years previously.</strong>');
+		    else if(t === '__' || t === 'End chapter 5')
+		    	p.remove();
+	    }
+    });
+    
     next();
 }
 
