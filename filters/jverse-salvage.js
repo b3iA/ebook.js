50730808cb7d1769294e07bab61505c9fa30d4d9
Mr. Black <gaia@national.shitposting.agency>
2015-12-22 00:57:42 +0100

    Updated the content filter to remove the &emsp;'s used in chapter 82 exclusively.

--- filters/jverse-salvage.js
+++ filters/jverse-salvage.js
@@ -9,6 +9,24 @@ function apply(params, next)
     if(fp.text() === 'END OF CHAPTER' || fp.text() === 'Chapter End')
         fp.remove();
 
+    if(chap.title === 'Dark Heart')
+    {
+    	var emsp_re = /&amp;emsp;&amp;emsp;/g;
+    	
+    	for(var pi = 0; pi < ps.length; pi++)
+    	{
+    		var cont = $(ps[pi]).contents();
+    		
+    		for(var i = 0; i < cont.length; i++)
+    		{
+    			var c = cont[i];
+    			
+    			if(c.type === 'text' && c.data.search(emsp_re) > -1)
+    				c.data = c.data.replace(emsp_re, '');
+    		}
+    	}
+    }
+    
     next();
 }
 
