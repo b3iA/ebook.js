d9113715b0eba3cda82a7481258bcfde466f0b82
Mr. Black <gaia@national.shitposting.agency>
2015-12-22 01:06:25 +0100

    Fixed a few unclosed tags in the HTML output filter.

--- filters/html.js
+++ filters/html.js
@@ -5,7 +5,7 @@ function apply(params, next)
 {
     var spec = params.spec;
     var oname = 'output/' + spec.filename + '.html';
-    var vspace = '\n    <p><br/</p><p><br/</p><p><br/</p>\n\n';
+    var vspace = '\n    <p><br/></p><p><br/></p><p><br/></p>\n\n';
     var html = [
         '<?xml version="1.0" encoding="utf-8"?>',
         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
