82af287dd2686e9c0d16e71f77aa6687bfde6f58
Mr. Black <gaia@national.shitposting.agency>
2016-01-04 05:42:19 +0100

    Updated content filter for salvage to remove emsp's only used in Dark Heart for paragraph indentation.

--- filters/jverse-salvage.js
+++ filters/jverse-salvage.js
@@ -6,6 +6,22 @@ function apply(params, next)
     var ps = $('p');
     var fp = $(ps[ps.length - 1]);
 
+    if(chap.title === 'Dark Heart')
+    {
+    	ps.each(function(idx, e)
+    	{
+    		var cont = $(e).contents();
+    		
+    		for(var i = 0; i < cont.length; i++)
+    		{
+    			var c = cont[i];
+    			
+    			if(c.type === 'text' && c.data.charCodeAt(0) === 0x2003)
+    				c.data = c.data.substr(2, c.data.length-2);
+    		}
+    	});
+    }
+    
     if(fp.text() === 'END OF CHAPTER' || fp.text() === 'Chapter End')
         fp.remove();
     
