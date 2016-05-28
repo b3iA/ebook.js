3ee03a3a06eebc41ef29014da9809b1181b8f824
Mr. Black <gaia@national.shitposting.agency>
2016-05-28 02:24:32 +0200

    Made a Jverse-Anthology following the reading order on request.

--- filters/jverse-good-training.js
+++ filters/jverse-good-training.js
@@ -3,20 +3,24 @@ function apply(params, next)
     var chap = params.chap;
 	var $ = chap.dom;
 	var rem = [];
+	var ps = $('p');
 	
-	if(chap.title === 'The Locals')
+	$('h2').each(function(i, e)
 	{
-		var ps = $('p');
-		
-		$('h2').each(function(i, e)
-		{
-			rem.push($(e));
-		});
+		rem.push($(e));
+	});
 	
+	if(chap.title === 'The Locals' || chap.title === 'Good Training: The Locals')
+	{
 		rem.push($(ps[0]));
 		rem.push($(ps[1]));
 	    rem.push($('p:contains("CONTINUED IN COMMENTS BELOW")'));
 	    rem.push($('p:contains("I felt like adding more. Have an epilogue!")'));
+	}
+	else if(chap.title === 'Saturday Morning Breakfast' || chap.title === 'Good Training: Saturday Morning Breakfast')
+	{
+		rem.push($(ps[0]));
+		rem.push($(ps[1]));
 	}
     
     if(rem.length)
