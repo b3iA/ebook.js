d9b8cd8dbffe0a5024cd651628989d35335b77db
Mr. Black <gaia@national.shitposting.agency>
2016-07-05 19:18:54 +0200

    Fixed epubcheck validation errors in epub.js:
    * Escaped chapter titles in headings and ToC
    * Added title tags to all pages including cover

--- filters/epub.js
+++ filters/epub.js
@@ -5,13 +5,22 @@ var fs = require('fs');
 // NOTES:
 // FBReader does not support text strikethrough (tags: s, del, strike)
 
+function escapeHTML(txt)
+{
+    return txt.replace('&', '&amp;')
+              .replace('"', '&quot;')
+              .replace('\'', '&apos;')
+              .replace('<', '&lt;')
+              .replace('>', '&gt;')
+}
+
 function createContents(spec, uuid)
 {
     var xml = [
         '<?xml version="1.0"?>',
         '<package version="2.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId">',
         '  <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">',
-        '    <dc:title>' + spec.title + '</dc:title>',
+        '    <dc:title>' + escapeHTML(spec.title) + '</dc:title>',
         '    <dc:language>en</dc:language>',
         '    <dc:identifier id="BookId" opf:scheme="UUID">' + uuid + '</dc:identifier>',
         '    <dc:creator opf:file-as="' + spec.creator + '" opf:role="aut">' + spec.creator + '</dc:creator>',
@@ -54,7 +63,7 @@ function createTOC(spec, uuid)
         '    <meta content="0" name="dtb:maxPageNumber"/>',
         '  </head>',
         '  <docTitle>',
-        '    <text>' + spec.title + '</text>',
+        '    <text>' + escapeHTML(spec.title) + '</text>',
         '  </docTitle>',
         '  <navMap>\n'].join('\n');
 
@@ -63,7 +72,7 @@ function createTOC(spec, uuid)
         xml += [
             '    <navPoint id="' + id + '" playOrder="' + ord + '">',
             '      <navLabel>',
-            '        <text>' + title + '</text>',
+            '        <text>' + escapeHTML(title) + '</text>',
             '      </navLabel>',
             '      <content src="' + id + '"/>',
             '    </navPoint>\n'
@@ -84,16 +93,18 @@ function createTOC(spec, uuid)
 
 function createXHTML(params, chap)
 {
+    var title = escapeHTML(chap.title);
     var xml = [
         '<?xml version="1.0" encoding="utf-8"?>',
         '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
         '<html xmlns="http://www.w3.org/1999/xhtml">',
         '  <head>',
+        '    <title>' + title + '</title>',
         '    <meta content="application/xhtml+xml; charset=utf-8" http-equiv="Content-Type"/>',
         '    <link href="style.css" rel="stylesheet" type="text/css"/>',
         '  </head>',
         '  <body>',
-        '    <h1>' + chap.title + '</h1>',
+        '    <h1>' + title + '</h1>',
         '    ' + (chap.byline ? '<p class="byline">By ' + chap.byline + '</p>' : '<p><br/></p><p><br/></p>'),
         '    <div class="chapter">\n'
     ].join('\n');
@@ -114,7 +125,7 @@ function createTitle(title)
 	var lines = title.split('\n');
 	
 	for(var i = 0; i < lines.length; i++)
-		html += '        <p class="center"><center><div class="title">' + lines[i] + '</div></center></p>\n';
+		html += '        <p class="center"><center><div class="title">' + escapeHTML(lines[i]) + '</div></center></p>\n';
 
 	return html;
 }
@@ -127,6 +138,7 @@ function createCover(params)
 		'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
 		'<html xmlns="http://www.w3.org/1999/xhtml">',
 		'    <head>',
+		'        <title>Cover</title>',
 		'        <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />',
 		'        <link rel="stylesheet" type="text/css" href="style.css" />',
 		'        <style type="text/css">',
