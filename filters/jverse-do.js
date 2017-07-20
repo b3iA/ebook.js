9cfc34904cb6f30ffdbdd8ff36ae070da34245be
Mr. Black <gaia@national.shitposting.agency>
2017-07-20 15:21:50 +0200

    Added spec and filter for 'Deathworld Origins'

---
+++ filters/jverse-do.js
@@ -0,0 +1,33 @@
+function apply(params, next)
+{
+    var chap = params.chap;
+    var $ = chap.dom;
+	var ps = $('p');
+	var rem = [];
+	
+    // Remove chapter links
+    $('p a').each(function(i, e)
+    {
+        var el = $(e);
+        
+        if(el.text() === 'Previous' || el.text() === 'Next' || el.text().indexOf('Chapter') === 0)
+        	rem.push(el);
+    });
+    
+    // Remove chapter headings
+    $('p').each(function(i, e)
+    {
+    	var el = $(e);
+    	
+    	if(el.text()[0] === '#')
+    		rem.push(el);
+    });
+    
+    params.purge(rem);
+    next();
+}
+
+module.exports =
+{
+    apply: apply
+};
