8555c9579d116a0172babcc800fc38f5aba6e125
Mr. Black <gaia@national.shitposting.agency>
2020-08-11 23:51:26 +0200

    Updated both Clint Stone filters to use utils.

--- filters/clint-stone-freedom.js
+++ filters/clint-stone-freedom.js
@@ -1,34 +1,33 @@
+const utils = require('./utils');
+
 function apply(params, next)
 {
-	var chap = params.chap;
-	var $ = chap.dom;
-	var rem = [];
-	var tr_re = /^translator note:/i;
-	var ct_re = /^continued in comments/i;
+	const chap = params.chap;
+	const $ = chap.dom;
+	const rem = [];
 	
-	$('p strong').each(function(i, e)
-	{
-		var el = $(e);
-
-		if(el.text().search(tr_re) === 0)
-			rem.push(el.parent());
-	});
-
-	$('p').each(function(i, e)
-	{
-		var el = $(e);
-
-		if(el.text().search(ct_re) === 0)
-			rem.push(el);
-	});
-
-	var rem_last_p = ['Stranger', 'Hand of War', 'Quest', 'Retribution', 'Fireproof', 'Greetings', 'The Feast', 'Undone', 'Susan', 'Lost Tales'];
-	var ps = $('p');
-	var lp = $(ps[ps.length - 1]);
-
-	if(rem_last_p.indexOf(chap.title) > -1 || lp.find('a').length)
+	utils.removeMatching($, rem, 'p strong', /^translator note:/i);
+	utils.removeMatching($, rem, 'p', /^continued in comments/i);
+	
+	const rem_last_p = [
+		'Stranger',
+		'Hand of War',
+		'Quest',
+		'Retribution',
+		'Fireproof',
+		'Greetings',
+		'The Feast',
+		'Undone',
+		'Susan',
+		'Lost Tales'
+	];
+	
+	const ps = $('p');
+	const lp = $(ps[ps.length - 1]);
+	
+	if(rem_last_p.includes(chap.title) || lp.find('a').length)
 		rem.push(lp);
-
+	
 	if(chap.title === 'Retribution')
 		rem.push($(ps[ps.length - 2]));
 	else if(chap.title === 'Greetings')
@@ -36,12 +35,12 @@ function apply(params, next)
 		rem.push($(ps[ps.length - 2]));
 		rem.push($(ps[ps.length - 3]));
 	}
-
+	
 	params.purge(rem);
 	next();
 }
 
 module.exports =
 {
-    apply: apply
+	apply: apply
 };
