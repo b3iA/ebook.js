b0d68318cd8789bd9d66b4615b804034841cb12b
Mr. Black <gaia@national.shitposting.agency>
2020-08-13 09:16:03 +0200

    Normalized whitespace to tabs in all spec generators.

--- update/savage-divinity.js
+++ update/savage-divinity.js
@@ -5,41 +5,41 @@ const spec = require('./spec.js');
 
 request({ uri: 'https://ruffwriterblog.wordpress.com/' }, (error, response, body) =>
 {
-    const $ = cheerio.load(body);
-    const content = $($('div.entry-content')[0]);
-    const tables = content.find('table');
-    const s = new spec.Spec();
-
-    s.title = 'Savage Divinity';
-    s.filename = 'Savage Divinity';
-    s.creator = 'RuffWriter';
-    s.filters = [
-        'from-url',
-        'savage-divinity',
-        'typography',
-        'finalize'
-    ];
-
-    for(let i = 3; i < tables.length; i += 2)
-    {
-        const table = $(tables[i]);
-        const title_tds = table.find('p');
-        const vol_title = $(title_tds[title_tds.length < 2 ? 0 : 1]).text().trim();
-        const chapters = $(tables[i+1]);
-        
-        chapters.find('tr').each((i0, tr) =>
-        {
-            $(tr).find('td > a').each((i1, a) =>
-            {
-                s.toc.push([
-                    vol_title + ': ' + a.children[0].data.trim(),
-                    a.attribs.href
-                ]);
-            });
-        });
-    }
-
-    s.writeTo('savage-divinity');
+	const $ = cheerio.load(body);
+	const content = $($('div.entry-content')[0]);
+	const tables = content.find('table');
+	const s = new spec.Spec();
+	
+	s.title = 'Savage Divinity';
+	s.filename = 'Savage Divinity';
+	s.creator = 'RuffWriter';
+	s.filters = [
+		'from-url',
+		'savage-divinity',
+		'typography',
+		'finalize'
+	];
+	
+	for(let i = 3; i < tables.length; i += 2)
+	{
+		const table = $(tables[i]);
+		const title_tds = table.find('p');
+		const vol_title = $(title_tds[title_tds.length < 2 ? 0 : 1]).text().trim();
+		const chapters = $(tables[i+1]);
+		
+		chapters.find('tr').each((i0, tr) =>
+		{
+			$(tr).find('td > a').each((i1, a) =>
+			{
+				s.toc.push([
+					vol_title + ': ' + a.children[0].data.trim(),
+					a.attribs.href
+				]);
+			});
+		});
+	}
+	
+	s.writeTo('savage-divinity');
 });
 
 
--- update/spec.js
+++ update/spec.js
@@ -4,34 +4,34 @@ const fs = require('fs');
 
 class Spec
 {
-    constructor()
-    {
-        this.title = 'Default Title';
-        this.creator = 'Default Creator';
-        this.filename = 'Default Filename';
-        this.filters = [];
-        this.toc = [];
-    }
-
-    writeTo(filename)
-    {
-        let spec = [
-            '{',
-            '   "title": "' + this.title + '",',
-            '   "creator": "' + this.creator + '",',
-            '   "filename": "' + this.filename + '",',
-            '   "output": ["html", "epub", "latex"],',
-            '   "filters":',
-            '   ['
-        ].join('\n');
-
-        spec += '\n' + this.filters.map((f) => '\t\t"' + f + '"').join(',\n') + '\n';
-        spec += '\t],\n\t"contents":\n\t[\n';
-        spec += this.toc.map((t) => '\t\t{\n\t\t\t"title": "' + t[0] + '",\n\t\t\t"src": "' + t[1] + '"\n\t\t}').join(',\n');
-        spec += '\n\t]\n}';
-
-        fs.writeFileSync(__dirname + '/../specs/' + filename + '.json', spec, 'utf-8');
-    }
+	constructor()
+	{
+		this.title = 'Default Title';
+		this.creator = 'Default Creator';
+		this.filename = 'Default Filename';
+		this.filters = [];
+		this.toc = [];
+	}
+	
+	writeTo(filename)
+	{
+		let spec = [
+			'{',
+			'   "title": "' + this.title + '",',
+			'   "creator": "' + this.creator + '",',
+			'   "filename": "' + this.filename + '",',
+			'   "output": ["html", "epub", "latex"],',
+			'   "filters":',
+			'   ['
+		].join('\n');
+		
+		spec += '\n' + this.filters.map((f) => '\t\t"' + f + '"').join(',\n') + '\n';
+		spec += '\t],\n\t"contents":\n\t[\n';
+		spec += this.toc.map((t) => '\t\t{\n\t\t\t"title": "' + t[0] + '",\n\t\t\t"src": "' + t[1] + '"\n\t\t}').join(',\n');
+		spec += '\n\t]\n}';
+		
+		fs.writeFileSync(__dirname + '/../specs/' + filename + '.json', spec, 'utf-8');
+	}
 }
 
 module.exports.Spec = Spec;
