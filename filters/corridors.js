52a988458188f7cddba3249d99307c6dc1ebe11d
Mr. Black <gaia@national.shitposting.agency>
2020-08-13 10:07:08 +0200

    Updated spec (3 new chapters) and filter for Corridors.

--- filters/corridors.js
+++ filters/corridors.js
@@ -7,9 +7,9 @@ function apply(params, next)
 	const rem = [];
 	
 	utils.pruneParagraphs(chap, rem, {
-		'Chapter 1: Pilot': [1, 0],
-		'Chapter 2: Haggling': [2, 0],
-		'Chapter 3: Incursion': [5, 0],
+		'Chapter 1: Pilot': [1, 1],
+		'Chapter 2: Haggling': [2, 1],
+		'Chapter 3: Incursion': [5, 1],
 		'Chapter 4: Home': [6, 0],
 		'Chapter 5: Intelligence': [7, 0],
 		'Chapter 6: Hermes': [8, 0],
@@ -25,7 +25,7 @@ function apply(params, next)
 		'Chapter 16: Whispers': [6, 0],
 		'Chapter 17: Revelations': [6, 0],
 		'Chapter 18: Flight (Part 1)': [6, 0],
-		'Chapter 18: Flight (Part 2)': [1, 0],
+		'Chapter 18: Flight (Part 2)': [1, 1],
 		'Chapter 19: Maneuvers (Part 1)': [6, 0],
 		'Chapter 19: Maneuvers (Part 2)': [1, 0],
 		'Chapter 20: Fracture': [6, 0],
@@ -33,8 +33,19 @@ function apply(params, next)
 		'Chapter 22: Exodus (Part 1)': [7, 0],
 		'Chapter 22: Exodus (Part 2)': [1, 0],
 		'Chapter 23: Schism (Part 1)': [7, 0],
-		'Chapter 23: Schism (Part 2)': [1, 0]
+		'Chapter 23: Schism (Part 2)': [1, 0],
+		'Chapter 24: Siege (Part 2)': [0, 1],
+		'Chapter 25: Cataclysm (Part 1/3)': [17, 1],
+		'Chapter 25: Cataclysm (Part 2/3)': [3, 1],
+		'Chapter 25: Cataclysm (Part 3/3)': [3, 1]
 	});
+	
+	utils.removeMatching($, rem, 'p', /^Corridors Wiki Page/gi); 
+	utils.removeMatching($, rem, 'p', /^Continued in Part/gi); 
+	utils.removeMatching($, rem, 'p', /^(\[)*Continued in the comments/gi); 
+	
+	if(chap.title == "Chapter 3: Incursion")
+		utils.removeMatching($, rem, 'p', /^The battle.*Continued in/gi); 
 	
 	params.purge(rem);
 	next();
