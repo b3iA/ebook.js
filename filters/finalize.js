be648ecb18bfabef824c61ff9a6dc83780e8983e
Mr. Black <gaia@national.shitposting.agency>
2020-11-29 14:37:31 +0100

    Improved the finalization filter to remove orphaned / widowed scene breaks

--- filters/finalize.js
+++ filters/finalize.js
@@ -1,25 +1,26 @@
-// The actions of other filters can leave the DOM in an undesirable state,
-// this filter attempts to correct these anomalies before final output processing.
-// It should always be used as the final stage.
-function apply(params, next)
+function check_for_invalid_break($, e)
 {
-	const $ = params.chap.dom;
-	
-	// Remove any empty paragraphs
-	$('p').each(function(i, e)
+	if(e.type === 'tag' && e.name === 'p')
 	{
-		const p = $(e);
+		const el = $(e);
 		
-		if(p.text().trim() === '')
-			p.remove();
-	});
+		if(el.hasClass('center') && el.text() === '&#x2042;')
+		{
+			el.remove();
+			return true;
+		}
+	}
 	
-	// Removal of DOM elements tends to leave surrounding
-	// newline text nodes, resulting in large gaps in the root.
+	return false;
+}
+
+function remove_whitespace($)
+{
 	const newl = /^\n*$/;
 	let   roots = $.root().contents();
 	let   rem = true;
 	
+
 	for(let i = 0; i < roots.length; i++)
 	{
 		const r = roots[i];
@@ -42,7 +43,44 @@ function apply(params, next)
 	
 	if(last.type === 'text' && last.data.search(newl) > -1)
 		$(last).remove();
+}
+
+// The actions of other filters can leave the DOM in an undesirable state,
+// this filter attempts to correct these anomalies before final output processing.
+// It should always be used as the final stage.
+function apply(params, next)
+{
+	const $ = params.chap.dom;
 	
+	// Remove any empty paragraphs
+	$('p').each(function(i, e)
+	{
+		const p = $(e);
+		
+		if(p.text().trim() === '')
+			p.remove();
+	});
+	
+	// Removal of DOM elements tends to leave surrounding
+	// newline text nodes, resulting in large gaps in the root.
+	remove_whitespace($);
+	
+	// Check for any widowed / orphaned scene breaks, and
+	// remove them if found.
+	let roots = $.root().contents();
+	let dirty = false;
+	
+	if(roots.length > 0 && check_for_invalid_break($, roots[0]))
+		dirty = true;
+		
+	if(roots.length > 1 && check_for_invalid_break($, roots[roots.length - 1]))
+		dirty = true;
+			
+	// If any were found and removed, we need to reensure that
+	// no unnecessary whitespace exists in the root.
+	if(dirty)
+		remove_whitespace($);
+
 	next();
 }
 
