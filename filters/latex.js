9af0421316420d9ea3dddecac8d3c397629fb9d4
Mr. Black <gaia@national.shitposting.agency>
2020-11-29 14:39:46 +0100

    Added table support to the LaTeX filter and fixed a bug: The author name was not being properly encoded before insertion into the document metadata.

--- filters/latex.js
+++ filters/latex.js
@@ -12,16 +12,116 @@ function l_esc(txt)
 			  .replace(/&/g, '\\&');
 }
 
+function replace_at(txt, idx, repl)
+{
+	return txt.substr(0, idx) + repl + txt.substr(idx + repl.length);
+}
+
+function newline_allowed(txt, idx)
+{
+	for(let b = idx - 1; b >= 0; b--)
+	{
+		if(txt[b] === '\n')
+			return false;
+		else if(!/(\s|\*|\\)/.test(txt[b]))
+			return true;
+		else if(txt[b] === '*' && b > 0 && txt[b-1] === '\\' && b > 1 && txt[b-2] === '\\')
+			return false;
+	}
+	
+	return true;
+}
+
 function filter(p, txt)
 {
-	return l_esc(p.unescape_html(txt).replace(/—/g, ' -- ')
-									 .replace(/–/g, '--'))
-									 .replace(/’/g, '\'')
-									 .replace(/\\([^%\$#_\{\}&])/gm, '{\\textbackslash}$1')
-									 .replace(/\\$/g, '{\\textbackslash}')
-									 .replace(/~/g, '{\\textasciitilde}')
-									 .replace(/\^/g, '{\\textasciicircum}')
-									 .replace(/…/g, '{\\ldots}');
+	return l_esc(p.unescape_html(txt)
+			.replace(/—/g, ' -- ')
+			.replace(/–/g, '--'))
+			.replace(/’/g, '\'')
+			.replace(/\\([^%\$#_\{\}&])/gm, '{\\textbackslash}$1')
+			.replace(/\\$/g, '{\\textbackslash}')
+			.replace(/~/g, '{\\textasciitilde}')
+			.replace(/\^/g, '{\\textasciicircum}')
+			.replace(/…/g, '{\\ldots}');
+}
+
+function get_colspan(cel)
+{
+	const cs = cel.attr('colspan');
+	
+	return cs !== undefined ? parseInt(cs, 10) : 1;
+}
+
+function table_tolatex(p, $, el)
+{
+	let l = '';
+	let mc = 0;
+	let cols = [];
+	
+	$('tr', el).each((i, e) =>
+	{
+		const cells = $('td', e);
+		const cc = cells.length;
+		let cidx = 0;
+		
+		cells.each((ci, ce) =>
+		{
+			const cel = $(ce);
+			const cs = get_colspan(cel);
+			const children = cel.children();
+			let align = 'L';
+			
+			if(children.length > 0 && children[0].type === 'tag' && children[0].name === 'center')
+				align = 'C';
+			
+			cols[cidx] = align;
+			cidx += cs;
+		});
+		
+		if(cc > mc)
+			mc = cc;
+	});
+	
+	l += '\\*\n\\begin{table}[h]\n\\centering\n\\begin{tabulary}{\\textwidth}{@{}' + cols.join('') + '@{}}\n\\toprule\n';
+	
+	$('tr', el).each((i, e) =>
+	{
+		let cl = '';
+		let cidx = 0;
+		let pure_bold = true;
+		
+		$('td', e).each((ci, ce) =>
+		{
+			const cel = $(ce);
+			const sub = cel.children();
+			const colspan = get_colspan(cel);
+			const centered = sub.length > 0 && sub[0].type === 'tag' && sub[0].name === 'center';
+			const target = $(centered ? sub[0] : ce);
+			const sub_t = target.children();
+			const bold = sub_t.length > 0 && sub_t[0].type === 'tag' && sub_t[0].name === 'strong';
+			const cont = tolatex(p, $, target).trim('\n').trim();
+			const is_last = cidx + colspan > mc - 1;
+			
+			if(cont !== '' && !bold)
+				pure_bold = false;
+			
+			if(colspan === 1)
+				cl += '\\pbox[t]{\\textwidth}{' + cont + '}';
+			else
+				cl += '\\multicolumn{' + colspan + '}{' + (centered ? 'c' : 'l') + (!is_last ? '|' : '') + '}{\\pbox[t]{\\textwidth}{' + cont + '}}';
+				
+			cidx += colspan;
+			
+			if(!is_last)
+				cl += ' & ';
+		});
+		
+		l += cl + ' \\\\' + (pure_bold ? ' \\midrule ' : '') + '\n';
+	});
+
+	l += '\\bottomrule\n\\end{tabulary}\\end{table}\n';
+	
+	return l;
 }
 
 function tolatex(p, $, e, brk)
@@ -31,67 +131,75 @@ function tolatex(p, $, e, brk)
 	e.contents().each(function(i, el)
 	{
 		const elem = $(el);
-		let   l = '';
-		
-		// console.log(id + el.type);
+		let l = '';
 		
 		if(el.type === 'text')
 			l += filter(p, el.data);
 		else if(el.type === 'tag')
 		{
+			const inner = el.name !== 'table' ? tolatex(p, $, elem) : undefined;
+			
+			if(inner === '')
+				return;
+			
 			if(el.name === 'em')
-				l += '\\textit{' + tolatex(p, $, elem) + '}';
+				l += '\\textit{' + inner + '}';
 			else if(el.name === 'strong')
-				l += '\\textbf{' + tolatex(p, $, elem) + '}';
+				l += '\\textbf{' + inner + '}';
 			else if(el.name === 'pre' || el.name === 'code')
-				l += '\\monosp{' + tolatex(p, $, elem).replace(/\n/g, '\\\\*') + '}';
+				l += '\\monosp{' + inner.replace(/\n{2,}/g, '\\\\[\\baselineskip]').replace(/\n/g, '\\\\*') + '}';
 			else if(el.name === 'a')
-				l += '\\href{' + l_esc(el.attribs['href']) + '}{' + tolatex(p, $, elem) + '}';
+				l += '\\href{' + l_esc(el.attribs['href']) + '}{' + inner + '}';
 			else if(el.name === 'p')
 			{
-				const t = tolatex(p, $, elem);
-				
-				if(elem.attr('class') === 'center')
+				if(elem.attr('class') !== 'center')
 				{
-					if(t === '⁂')
-						l += '\\asterism\n';
-					else
-						l += '\\begin{center}' + t + '\\end{center}';
+					const align_r = elem.attr('align') === 'right';
+					
+					if(align_r)
+						l+= '\\vspace{1em}\\hspace*{\\fill}\\begin{minipage}{0.75\\textwidth}\\begin{flushright}';
+					
+					l += newline_allowed(inner, inner.length) ? inner + '\\vspace{1em}\\\\*\n' : inner;
+					
+					if(align_r)
+						l += '\\end{flushright}\\end{minipage}';
 				}
 				else
 				{
-					let tmp = t.replace(/\n\n?/g, '\n') + (t.indexOf('\\star') > -1 ? '' : '\n');
-					
-					if(tmp.length > 1 && (tmp[tmp.length-1] !== '\n' || tmp[tmp.length-2] !== '\n'))
-						tmp += '\n';
-					
-					l += tmp;
+					if(inner === '⁂')
+						l += '\\asterism\n';
+					else
+						l += '\\begin{center}' + inner + '\\end{center}';
 				}
 			}
 			else if(el.name === 'blockquote')
-				l += '\\begin{displayquote}\n' + tolatex(p, $, elem) + '\n\\end{displayquote}';
+				l += '\\begin{displayquote}\n' + inner + '\n\\end{displayquote}';
 			else if(el.name === 'span')
-				l += tolatex(p, $, elem);
+				l += inner;
 			else if(el.name === 'li')
-				l += '\\item ' + tolatex(p, $, elem);
+				l += '\\item ' + inner;
 			else if(el.name === 'ul')
-				l += '\\begin{itemize}' + tolatex(p, $, elem) + '\n\\end{itemize}';
+				l += '\\begin{itemize}' + inner + '\n\\end{itemize}';
 			else if(el.name === 'ol')
-				l += '\\begin{enumerate}' + tolatex(p, $, elem) + '\n\\end{enumerate}';
-			else if(el.name === 'br')
+				l += '\\begin{enumerate}' + inner + '\n\\end{enumerate}';
+			else if(el.name === 'br' && newline_allowed(l, l.length))
 				l += '\\\\*\n';
+			else if(el.name === 'center')
+				l += '\\begin{center}' + inner + '\\end{center}';
 			else if(el.name === 's' || el.name === 'del' || el.name === 'strike')
-				l += '\\sout{' + tolatex(p, $, elem) + '}';
+				l += '\\sout{' + inner + '}';
 			else if(el.name === 'sup')
-				l += '\\textsuperscript{' + tolatex(p, $, elem) + '}';
+				l += '\\textsuperscript{' + inner + '}';
+			else if(el.name === 'table')
+				l += table_tolatex(p, $, elem);
 			else
 			{
 				console.log('LaTeX: Unhandled tag: ' + el.name);
-				l += tolatex(p, $, elem);
+				l += inner;
 			}
 		}
 		
-		latex += el.type !== 'tag' || el.name !== 'p' ? l.replace(/\n\n?/g, '\n') : l;
+		latex += l;
 	});
 	
 	return latex;
@@ -126,14 +234,19 @@ function apply(params, next)
 		'\\usepackage{microtype}',
 		'\\usepackage{needspace}',
 		'\\usepackage{ifthen}',
+		'\\usepackage{tabulary}',
+		'\\usepackage{booktabs}',
+		'\\usepackage{pbox}',
 		'',
 		'\\title{\\textsc{' + title.replace(n_re, '\\\\\n') + '}}',
 		'\\author{\\textsc{By ' + creator + '}' + (spec.patreon ? '\\\\ \\small{Donate securely to the author at \\href{' + l_esc(spec.patreon) + '}{Patreon}}' : '') + '}',
 		'\\date{}',
 		'',
+		'\\errorcontextlines4',
+		'',
 		'\\hypersetup{',
 		'  pdftitle = {' + title.replace(n_re, ' - ') + '},',
-		'  pdfauthor = {' + spec.creator + '},',
+		'  pdfauthor = {' + creator + '},',
 		'  pdfproducer = {EbookJS},',
 		'  colorlinks = true,',
 		'  linkcolor = [rgb]{0.09,0.15,0.588},',
@@ -173,6 +286,7 @@ function apply(params, next)
 		'\\renewcommand{\\cftsecaftersnum}{\\end{lrbox}}',
 		'\\renewcommand{\\cftsecleader}{\\cftdotfill{\\cftdotsep}}',
 		'\\renewcommand{\\contentsname}{\\textsc{Contents}\\linebreak}',
+		'\\renewcommand{\\arraystretch}{1.2}',
 		'\\setcounter{secnumdepth}{-2}',
 		'',
 		'\\begin{document}',
@@ -211,7 +325,7 @@ function apply(params, next)
 		latex += tolatex(params, chap.dom, chap.dom.root());
 	}
 	
-	latex += '\\end{document}'
+	latex += '\n\\end{document}'
 	
 	fs.writeFileSync(oname, latex, 'utf-8');
 	next();
